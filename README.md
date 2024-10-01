76ee7fa81b943914778b27c0f6445a9fca434af6
vijay-fs <vijaykarthik9868@gmail.com>
2024-10-01 13:53:45 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# kriyadocs JSON Schema View Builder documentation
+
+This is documentation for Kriyadocs JSON schema View builder. The builder will render views based on JSON schema and arrange elements on the screen according to the definition.
+
+The screens can be broken down to the following parts:
+
+- Layout view (12 column grid)
+- Sections
+- Elements
+- Actions
+
+## Local Development
+
+First, run `yarn install` to install the dependencies.
+
+Then, run `yarn dev` to start the development server and visit localhost:3000.
---
+++ theme.config.jsx
@@ -0,0 +1,42 @@
+export default {
+  logo: <span>Github</span>,
+  project: {
+    link: "https://github.com/vijay-fs",
+    icon: (
+      <svg width="24" height="24" viewBox="0 0 256 256">
+        <path
+          fill="currentColor"
+          d="m231.9 169.8l-94.8 65.6a15.7 15.7 0 0 1-18.2 0l-94.8-65.6a16.1 16.1 0 0 1-6.4-17.3L45 50a12 12 0 0 1 22.9-1.1L88.5 104h79l20.6-55.1A12 12 0 0 1 211 50l27.3 102.5a16.1 16.1 0 0 1-6.4 17.3Z"
+        ></path>
+      </svg>
+    )
+  },
+  // ... other theme options
+  footer: {
+    text: (
+      <span className="nx-items-center">
+        Powered by
+        <img
+          alt="Coffee"
+          style={{ height: "2.5rem", width: "auto" }}
+          src="https://coffeeinc.in/assets/coffee_2023_low_res.png"
+        ></img>
+      </span>
+    ),
+  },
+  search: {
+    placeholder: "Search docs...",
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: 2,
+  },
+  feedback: {
+    content: null,
+  },
+  editLink: {
+    text: null,
+  },
+
+
+
+};
